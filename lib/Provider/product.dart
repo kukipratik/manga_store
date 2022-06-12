cc1c35cf01ad1d0c8c70773caf1c5acc3419ad49
kukipratik <kukipratik123@gmail.com>
2022-06-12 11:09:14 +0545

    tut 251 done

--- lib/Provider/product.dart
+++ lib/Provider/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String? id;
@@ -17,8 +20,32 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    bool oldStatus = isFavorite;
+
+    //this is for mobile ui....
     isFavorite = !isFavorite;
     notifyListeners();
+
+    //and this block is for web server... (Note:- error is thrown for only post and get.)
+    final url =
+        'https://backend-practice-23eef-default-rtdb.firebaseio.com/products/$id.json';
+    try {
+      var response = await http.patch(Uri.parse(url),
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+      if (response.statusCode >= 400) {
+        //will be helpful if any error occurs in server side...
+        isFavorite = oldStatus;
+        notifyListeners();
+      }
+    } catch (error) {
+      //this catch will be helpful when no network...
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
+
+    // print(response.body);
   }
 }
